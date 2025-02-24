3c844ac1ee48bfaaaa149391c92c24197b89fac1
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2025-02-24 16:05:13 +0400

    Added link to navbar + css cleaning

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ async function init() {
   document.querySelector("#cards").innerHTML = "";
   json.forEach((element) => {
     document.querySelector("#cards").innerHTML += `
-    <div class="card mb-3" onclick="window.location.href='${element.link}'" style="width: 25%; cursor: pointer">
+    <div class="card mb-3" style="width: 25%; cursor: pointer">
+    <a href='${element.link}'></a>
   <div class="card-body">
     <div class="card mb-3" href="${element.image}">
     <img src="${element.image}"/>
+  </div>
   </div>
   <div class="card-footer text-muted">
     <h4 class="card-title">${element.name}</h4>
        <h6 class="card-subtitle mb-2 text-muted">${element.description}</h6>
   </div>
 </div>
-</a>
+
     `;
   });
 }
