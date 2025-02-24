f1ee5239631ce27d884a5f42c8355b66e77eac5a
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2025-02-24 16:15:38 +0400

    Revert "css cleaning"
    This reverts commit 8a42457d8eb3779ec446d5effcd4913c967c5cef.

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ async function init() {
   json.forEach((element) => {
     document.querySelector("#cards").innerHTML += `
     <div class="card mb-3" style="width: 25%; cursor: pointer">
-    <a href='${element.link}' class="m-0 w-100 h-100">
-  <div class="card-body h-75 m-0 w-100">
-    <div class="card mb-3 " href="${element.image}">
+    <a href='${element.link}'></a>
+  <div class="card-body">
+    <div class="card mb-3" href="${element.image}">
     <img src="${element.image}"/>
   </div>
   </div>
-  <div class="card-footer text-muted h-25 m-0 w-100">
+  <div class="card-footer text-muted">
     <h4 class="card-title">${element.name}</h4>
        <h6 class="card-subtitle mb-2 text-muted">${element.description}</h6>
   </div>
-  </a>
 </div>
 
     `;
