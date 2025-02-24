5a6ef6b841333453c685d8ac61c0a3212cdd382e
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2025-02-24 16:24:33 +0400

    Corrected href link

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ async function init() {
   json.forEach((element) => {
     document.querySelector("#cards").innerHTML += `
     <div class="card mb-3" style="width: 25%; cursor: pointer">
-    <a href='${element.link}'></a>
-  <div class="card-body">
-    <div class="card mb-3" href="${element.image}">
-    <img src="${element.image}"/>
-  </div>
-  </div>
-  <div class="card-footer text-muted">
-    <h4 class="card-title">${element.name}</h4>
-       <h6 class="card-subtitle mb-2 text-muted">${element.description}</h6>
-  </div>
-</div>
+      <a href='${element.link}'>
+        <div class="card-body">
+          <div class="card mb-3" href="${element.image}">
+            <img src="${element.image}"/>
+          </div>
+        </div>
+        <div class="card-footer text-muted">
+          <h4 class="card-title">${element.name}</h4>
+            <h6 class="card-subtitle mb-2 text-muted">${element.description}</h6>
+        </div>
+      </a>
+    </div>
 
     `;
   });
